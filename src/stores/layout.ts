0cf353d7cde1a956f7ab232cd8f65e0fe92cc609
wflixu <wflixu@88.com>
2024-01-26 14:57:41 +0800

    添加背景设置

--- src/stores/layout.ts
+++ src/stores/layout.ts
@@ -1,15 +1,23 @@
 import { ref, computed } from "vue";
 import { defineStore } from "pinia";
+import defaultBgImg from '../assets/imgs/bg.jpg'
+const cachedBgUrl = window.localStorage.getItem('bg')
+
 
 export const useLayoutStore = defineStore("layout", () => {
   const showBookmark = ref(false);
   const showBackend = ref(false);
 
+  const bgurl = ref(cachedBgUrl ?? defaultBgImg)
+  const setBgurl = (url: string) => {
+    bgurl.value = url;
+    window.localStorage.setItem('bg', url)
+  }
   const toggleShowBookmark = (v?: boolean) => {
     showBookmark.value = v ?? !showBookmark.value;
   };
   const toggleShowBackend = (v?: boolean) => {
     showBackend.value = v ?? !showBackend.value;
   };
-  return { showBookmark, showBackend, toggleShowBookmark, toggleShowBackend };
+  return { showBookmark, showBackend, toggleShowBookmark, toggleShowBackend, bgurl, setBgurl };
 });
---
+++ src/views/settings/Settings.vue
@@ -0,0 +1,66 @@
+<template>
+    <div class="settings " :class="{ 'expand': open }">
+        <span class="icon" @click="onToggle">
+            <SettingOutlined :style="iconStyle" />
+        </span>
+        <a-drawer v-model:open="open" title="设置" :width="810" placement="right" @after-open-change="afterOpenChange">
+            <a-tabs v-model:activeKey="activeKey" type="card">
+                <a-tab-pane key="bg" tab="背景">
+                   
+                </a-tab-pane>
+            </a-tabs>
+            <template v-if="activeKey == 'bg'">
+                <ConfigBackground />
+            </template>
+        </a-drawer>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { SettingOutlined } from "@ant-design/icons-vue"
+import ConfigBackground from './ConfigBackground.vue'
+import { ref } from "vue";
+
+
+const open = ref<boolean>(false);
+const iconStyle = {
+    fontSize: '24px',
+    color: '#eee'
+}
+
+const activeKey = ref('bg')
+
+const onToggle = () => {
+    open.value = !open.value
+
+}
+
+const afterOpenChange = (bool: boolean) => {
+    console.log('open', bool);
+};
+
+</script>
+
+<style scoped>
+.settings {
+    position: fixed;
+    width: 42px;
+    height: 42px;
+    bottom: 24px;
+    left: 24px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    background-color: rgba(1, 1, 1, 0.4);
+    border-radius: 4px;
+    cursor: pointer;
+
+    .icon {
+        display: inline-flex;
+        justify-content: center;
+        align-items: center;
+        width: 100%;
+        height: 100%;
+    }
+}
+</style>
