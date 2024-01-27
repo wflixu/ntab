fea2a268d30b536de1189f5919d2fe07763c1e30
wflixu <wflixu@88.com>
2024-01-27 12:28:29 +0800

    [feat] remove bookmarks and add commands tips

---
+++ src/views/home/CommandTips.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="command-tips">
+        <div class="item">
+             <span>显示书签</span>
+             <span>⌘ + ⇧ + B</span>
+        </div>
+        <div class="item">
+             <span>历史记录</span>
+             <span>⌘ + Y</span>
+        </div>
+    </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style scoped>
+.command-tips {
+    position: fixed;
+    top:16px;
+    right: 24px;
+
+}
+.item {
+    height: 32px;
+    color: #aaa;
+    display: flex;
+    font-size: 18px;
+    justify-content: space-between;
+    align-items: center;
+    width: 200px;
+}
+</style>
